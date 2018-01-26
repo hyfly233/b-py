999343e065653ff58eed3a94817484670eeca3aa
hyfly233 <hyfly233@outlook.com>
2018-01-26 12:00:00 +0800

    commit

--- a2a_demo/common/client/client.py
+++ a2a_demo/common/client/client.py
@@ -1,4 +1,7 @@
+import json
 from typing import Any
+
+import httpx
 
 from a2a_demo.common.types import AgentCard
 
@@ -15,3 +18,19 @@ class A2AClient:
     async def send_task(self, payload: dict[str, Any]) -> SendTaskResponse:
         request = SendTaskRequest(params=payload)
         return SendTaskResponse(**await self._send_request(request))
+
+    async def send_task_streaming(
+        self, payload: dict[str, Any]
+    ) -> AsyncIterable[SendTaskStreamingResponse]:
+        request = SendTaskStreamingRequest(params=payload)
+        with httpx.Client(timeout=None) as client:
+            with connect_sse(
+                client, "POST", self.url, json=request.model_dump()
+            ) as event_source:
+                try:
+                    for sse in event_source.iter_sse():
+                        yield SendTaskStreamingResponse(**json.loads(sse.data))
+                except json.JSONDecodeError as e:
+                    raise A2AClientJSONError(str(e)) from e
+                except httpx.RequestError as e:
+                    raise A2AClientHTTPError(400, str(e)) from e
