88e127db491088d3ab9678921a2c4cf96cdbbc5c
hyfly233 <hyfly233@outlook.com>
2018-03-11 12:00:00 +0800

    commit

--- table_transformer_demo/main.py
+++ table_transformer_demo/main.py
@@ -1,3 +1,11 @@
 from transformers import pipeline
 
 pipe = pipeline("object-detection", model="microsoft/table-transformer-detection")
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
