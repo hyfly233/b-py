a23b382dbb937e9f4232cde02c13e4f09939b49e
hyfly233 <hyfly233@outlook.com>
2018-03-12 12:00:00 +0800

    commit

--- table_transformer_demo/main.py
+++ table_transformer_demo/main.py
@@ -1,10 +1,21 @@
+from PIL import Image
+from huggingface_hub import hf_hub_download
+from transformers import DetrFeatureExtractor
 from transformers import pipeline
 
 pipe = pipeline("object-detection", model="microsoft/table-transformer-detection")
 
-
 def main():
-    pass
+    file_path = hf_hub_download(repo_id="nielsr/example-pdf", repo_type="dataset", filename="example_pdf.png")
+    image = Image.open(file_path).convert("RGB")
+    width, height = image.size
+    image.resize((int(width * 0.5), int(height * 0.5)))
+
+    feature_extractor = DetrFeatureExtractor()
+    encoding = feature_extractor(image, return_tensors="pt")
+    encoding.keys()
+
+    print(encoding['pixel_values'].shape)
 
 
 if __name__ == '__main__':
