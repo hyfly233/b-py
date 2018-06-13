1544ecf0806df7e4d84c0c5310014f010796ab51
hyfly233 <hyfly233@outlook.com>
2018-06-13 12:00:00 +0800

    commit

--- a2a_demo/agents/agent.py
+++ a2a_demo/agents/agent.py
@@ -4,3 +4,26 @@ from typing import Any, AsyncIterable, Dict, Optional
 
 
 request_ids = set()
+
+
+def create_request_form(date: Optional[str] = None, amount: Optional[str] = None, purpose: Optional[str] = None) -> \
+dict[str, Any]:
+    """
+     Create a request form for the employee to fill out.
+
+     Args:
+         date (str): The date of the request. Can be an empty string.
+         amount (str): The requested amount. Can be an empty string.
+         purpose (str): The purpose of the request. Can be an empty string.
+
+     Returns:
+         dict[str, Any]: A dictionary containing the request form data.
+     """
+    request_id = "request_id_" + str(random.randint(1000000, 9999999))
+    request_ids.add(request_id)
+    return {
+        "request_id": request_id,
+        "date": "<transaction date>" if not date else date,
+        "amount": "<transaction dollar amount>" if not amount else amount,
+        "purpose": "<business justification/purpose of the transaction>" if not purpose else purpose,
+    }
