e0378db927f1524f9f03a729a2058becaf78475f
hyfly233 <hyfly233@outlook.com>
2018-06-16 12:00:00 +0800

    commit

--- a2a_demo/agents/agent.py
+++ a2a_demo/agents/agent.py
@@ -27,3 +27,59 @@ dict[str, Any]:
         "amount": "<transaction dollar amount>" if not amount else amount,
         "purpose": "<business justification/purpose of the transaction>" if not purpose else purpose,
     }
+
+
+def return_form(
+        form_request: dict[str, Any],
+        tool_context: ToolContext,
+        instructions: Optional[str] = None) -> dict[str, Any]:
+    """
+     Returns a structured json object indicating a form to complete.
+
+     Args:
+         form_request (dict[str, Any]): The request form data.
+         tool_context (ToolContext): The context in which the tool operates.
+         instructions (str): Instructions for processing the form. Can be an empty string.
+
+     Returns:
+         dict[str, Any]: A JSON dictionary for the form response.
+     """
+    if isinstance(form_request, str):
+        form_request = json.loads(form_request)
+
+    tool_context.actions.skip_summarization = True
+    tool_context.actions.escalate = True
+    form_dict = {
+        'type': 'form',
+        'form': {
+            'type': 'object',
+            'properties': {
+                'date': {
+                    'type': 'string',
+                    'format': 'date',
+                    'description': 'Date of expense',
+                    'title': 'Date',
+                },
+                'amount': {
+                    'type': 'string',
+                    'format': 'number',
+                    'description': 'Amount of expense',
+                    'title': 'Amount',
+                },
+                'purpose': {
+                    'type': 'string',
+                    'description': 'Purpose of expense',
+                    'title': 'Purpose',
+                },
+                'request_id': {
+                    'type': 'string',
+                    'description': 'Request id',
+                    'title': 'Request ID',
+                },
+            },
+            'required': list(form_request.keys()),
+        },
+        'form_data': form_request,
+        'instructions': instructions,
+    }
+    return json.dumps(form_dict)
