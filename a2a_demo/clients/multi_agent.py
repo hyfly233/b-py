465992875f4a3a5c13c8d4deddfa6b4d5a39213a
hyfly233 <hyfly233@outlook.com>
2018-07-23 12:00:00 +0800

    commit

---
+++ a2a_demo/clients/multi_agent.py
@@ -0,0 +1,30 @@
+import json
+from typing import List
+
+from a2a_demo.common.client import A2ACardResolver
+from a2a_demo.common.types import AgentCard
+
+
+class HostAgent:
+    """
+    主机代理。该代理负责选择要向其发送任务的远程代理，并协调其工作。
+    """
+
+    def __init__(
+            self,
+            remote_agent_addresses: List[str],
+            task_callback: TaskUpdateCallback | None = None
+    ):
+        self.task_callback = task_callback
+        self.remote_agent_connections: dict[str, RemoteAgentConnections] = {}
+        self.cards: dict[str, AgentCard] = {}
+        for address in remote_agent_addresses:
+            card_resolver = A2ACardResolver(address)
+            card = card_resolver.get_agent_card()
+            remote_connection = RemoteAgentConnections(card)
+            self.remote_agent_connections[card.name] = remote_connection
+            self.cards[card.name] = card
+        agent_info = []
+        for ra in self.list_remote_agents():
+            agent_info.append(json.dumps(ra))
+        self.agents = '\n'.join(agent_info)
