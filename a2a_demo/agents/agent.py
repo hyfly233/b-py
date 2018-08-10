7d709e5adb73cc27c1adf663c5dc3164a9ec94cf
hyfly233 <hyfly233@outlook.com>
2018-08-10 12:00:00 +0800

    commit

--- a2a_demo/agents/agent.py
+++ a2a_demo/agents/agent.py
@@ -1,11 +1,9 @@
-import json
-import random
-from typing import Any, Optional, AsyncIterable, Dict, Iterator
+from typing import Any, AsyncIterable, Dict, Iterator
 
-from google.adk.tools import ToolContext
 from langchain.agents import AgentExecutor, create_react_agent
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableConfig
+from langchain_core.tools import tool
 from langchain_ollama import OllamaLLM
 
 from langchain_agent.lang_chain_agent import StreamingCallback
@@ -13,92 +11,9 @@ from langchain_agent.lang_chain_agent import StreamingCallback
 request_ids = set()
 
 
-def create_request_form(date: Optional[str] = None,
-                        amount: Optional[str] = None,
-                        purpose: Optional[str] = None) -> dict[str, Any]:
-    """
-     Create a request form for the employee to fill out.
-
-     Args:
-         date (str): The date of the request. Can be an empty string.
-         amount (str): The requested amount. Can be an empty string.
-         purpose (str): The purpose of the request. Can be an empty string.
-
-     Returns:
-         dict[str, Any]: A dictionary containing the request form data.
-     """
-    request_id = "request_id_" + str(random.randint(1000000, 9999999))
-    request_ids.add(request_id)
-    return {
-        "request_id": request_id,
-        "date": "<transaction date>" if not date else date,
-        "amount": "<transaction dollar amount>" if not amount else amount,
-        "purpose": "<business justification/purpose of the transaction>" if not purpose else purpose,
-    }
-
-
-def return_form(
-        form_request: dict[str, Any],
-        tool_context: ToolContext,
-        instructions: Optional[str] = None) -> str:
-    """
-     Returns a structured json object indicating a form to complete.
-
-     Args:
-         form_request (dict[str, Any]): The request form data.
-         tool_context (ToolContext): The context in which the tool operates.
-         instructions (str): Instructions for processing the form. Can be an empty string.
-
-     Returns:
-         dict[str, Any]: A JSON dictionary for the form response.
-     """
-    if isinstance(form_request, str):
-        form_request = json.loads(form_request)
-
-    tool_context.actions.skip_summarization = True
-    tool_context.actions.escalate = True
-    form_dict = {
-        'type': 'form',
-        'form': {
-            'type': 'object',
-            'properties': {
-                'date': {
-                    'type': 'string',
-                    'format': 'date',
-                    'description': 'Date of expense',
-                    'title': 'Date',
-                },
-                'amount': {
-                    'type': 'string',
-                    'format': 'number',
-                    'description': 'Amount of expense',
-                    'title': 'Amount',
-                },
-                'purpose': {
-                    'type': 'string',
-                    'description': 'Purpose of expense',
-                    'title': 'Purpose',
-                },
-                'request_id': {
-                    'type': 'string',
-                    'description': 'Request id',
-                    'title': 'Request ID',
-                },
-            },
-            'required': list(form_request.keys()),
-        },
-        'form_data': form_request,
-        'instructions': instructions,
-    }
-    return json.dumps(form_dict)
-
-
-def reimburse(request_id: str) -> dict[str, Any]:
-    """Reimburse the amount of money to the employee for a given request_id."""
-    if request_id not in request_ids:
-        return {"request_id": request_id, "status": "Error: Invalid request_id."}
-    return {"request_id": request_id, "status": "approved"}
-
+@tool
+def tool_echo_hello_world() -> str:
+    return "Java - Hello, World!"
 
 class TestOllamaAgent:
 
