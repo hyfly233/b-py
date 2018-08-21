05c20f2aa558c195335f0e47d16a052b48a2e7ab
hyfly233 <hyfly233@outlook.com>
2018-08-21 12:00:00 +0800

    commit

--- a2a_demo/common/server/__init__.py
+++ a2a_demo/common/server/__init__.py
@@ -1,4 +1,4 @@
-from .server import A2AServer
+from .a2a_server import A2AServer
 from .task_manager import TaskManager, InMemoryTaskManager
 
 __all__ = ["A2AServer", "TaskManager", "InMemoryTaskManager"]
