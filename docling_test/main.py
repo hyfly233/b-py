441a535f2e5254e957683fa174d8cdcbf5ec92ba
hyfly233 <hyfly233@outlook.com>
2018-08-31 12:00:00 +0800

    commit

--- docling_test/main.py
+++ docling_test/main.py
@@ -3,10 +3,12 @@ from docling.datamodel.pipeline_options import PdfPipelineOptions
 from docling.document_converter import DocumentConverter, PdfFormatOption
 
 pipeline_options = PdfPipelineOptions()
-pipeline_options.generate_picture_images = True
-pipeline_options.images_scale = 2
-pipeline_options.do_picture_classification = True
-pipeline_options.do_picture_description = True
+pipeline_options.do_formula_enrichment = True
+
+# pipeline_options.generate_picture_images = True
+# pipeline_options.images_scale = 2
+# pipeline_options.do_picture_classification = True
+# pipeline_options.do_picture_description = True
 
 converter = DocumentConverter(format_options={
     InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)
