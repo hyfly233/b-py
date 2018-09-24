43cd6452efbe3f44d5d0b8cbf75a33009e335ee8
hyfly233 <hyfly233@outlook.com>
2018-09-24 12:00:00 +0800

    commit

---
+++ docling_demo/main.py
@@ -0,0 +1,75 @@
+import logging
+import time
+from pathlib import Path
+
+from docling.datamodel.base_models import InputFormat
+from docling.datamodel.pipeline_options import (
+    AcceleratorDevice,
+    AcceleratorOptions,
+    PdfPipelineOptions,
+)
+from docling.document_converter import DocumentConverter, PdfFormatOption
+from docling.chunking import HybridChunker
+
+SOURCE = "./pdf/Kotlin实战.pdf"
+# SOURCE = "./pdf/OpsGuide-Network-Troubleshooting.pdf"
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s"
+)
+_log = logging.getLogger(__name__)
+
+
+def main():
+    _log.info(f"---------")
+
+    # 配置分块
+    chunker = HybridChunker()
+
+
+    # 配置 pipeline
+    pipeline_options = PdfPipelineOptions()
+    pipeline_options.do_ocr = True
+    pipeline_options.do_table_structure = True
+    pipeline_options.table_structure_options.do_cell_matching = True
+    ## 加速配置
+    pipeline_options.accelerator_options = AcceleratorOptions(
+        num_threads=8, device=AcceleratorDevice.MPS
+    )
+
+    converter = DocumentConverter(
+        format_options={
+            InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)
+        }
+    )
+
+    _log.info(f"转换器配置完成 ..........")
+
+    start_time = time.time()
+    # 格式化时间
+    _log.info(f"开始转换文档，开始时间 [{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time))}] ..........")
+
+    # 转换文档
+    input_doc_path = Path(SOURCE)
+    conv_result = converter.convert(input_doc_path)
+
+    end_time = time.time()
+    _log.info(f"转换文档结束，完成时间 [{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(end_time))}] ..........")
+    _log.info(f"转换文档耗时 [{end_time - start_time}] ..........")
+
+    # 导出
+    output_dir = Path("output")
+    output_dir.mkdir(parents=True, exist_ok=True)
+    doc_filename = conv_result.input.file.stem
+
+    _log.info("开始导出结果到 Markdown ..........")
+
+    # 导出到 markdown
+    with (output_dir / f"{doc_filename}.md").open("w", encoding="utf-8") as fp:
+        fp.write(conv_result.document.export_to_markdown())
+
+    _log.info("导出结果到 Markdown 完成 ..........")
+
+
+if __name__ == '__main__':
+    main()
