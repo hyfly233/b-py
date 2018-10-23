b0da968171495692a2fefe8d82d307f3c5770f36
hyfly233 <hyfly233@outlook.com>
2018-10-23 12:00:00 +0800

    commit

--- docling_demo/main.py
+++ docling_demo/main.py
@@ -14,12 +14,14 @@ from docling.datamodel.pipeline_options import (
 )
 from docling.document_converter import DocumentConverter, PdfFormatOption
 from docling_core.transforms.chunker.hybrid_chunker import HybridChunker
+from docling_core.transforms.chunker.tokenizer.huggingface import HuggingFaceTokenizer
 
 # from docling.pipeline.vlm_pipeline import VlmPipeline
 
-SOURCE = "./pdf/2206.01062v1-表格图片.pdf"
+# SOURCE = "./pdf/2206.01062v1-表格图片.pdf"
 # SOURCE = "./pdf/2503.00004v1-大量公式.pdf"
 # SOURCE = "./pdf/2504.20485v1-代码.pdf"
+SOURCE = "/Users/flyhy/Documents/test01.pdf"
 
 
 logging.basicConfig(
@@ -116,48 +118,55 @@ def main():
 
     _log.info(f"转换器配置完成 ..........")
 
-    start_time = time.time()
-    # 格式化时间
-    _log.info(f"开始转换文档，开始时间 [{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time))}] ..........")
+    try:
+        start_time = time.time()
+        # 格式化时间
+        _log.info(
+            f"开始转换文档，开始时间 [{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time))}] ..........")
 
-    # 转换文档
-    input_doc_path = Path(SOURCE)
-    conv_result = converter.convert(input_doc_path)
+        # 转换文档
+        input_doc_path = Path(SOURCE)
+        conv_result = converter.convert(input_doc_path)
 
-    end_time = time.time()
-    _log.info(f"转换文档结束，完成时间 [{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(end_time))}] ..........")
-    _log.info(f"转换文档耗时 [{end_time - start_time}] s ..........")
+        end_time = time.time()
+        _log.info(f"转换文档结束，完成时间 [{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(end_time))}] ..........")
+        _log.info(f"转换文档耗时 [{end_time - start_time}] s ..........")
 
-    # 导出
-    output_dir = Path("output")
-    output_dir.mkdir(parents=True, exist_ok=True)
-    doc_filename = conv_result.input.file.stem
+        # 导出
+        output_dir = Path("output")
+        output_dir.mkdir(parents=True, exist_ok=True)
+        doc_filename = conv_result.input.file.stem
 
-    _log.info("开始导出结果到 Markdown ..........")
+        _log.info("开始导出结果到 Markdown ..........")
 
-    # 导出到 markdown
-    with (output_dir / f"{doc_filename}.md").open("w", encoding="utf-8") as fp:
-        fp.write(conv_result.document.export_to_markdown())
+        # 导出到 markdown
+        with (output_dir / f"{doc_filename}.md").open("w", encoding="utf-8") as fp:
+            fp.write(conv_result.document.export_to_markdown())
 
-    _log.info("导出结果到 Markdown 完成 ..........")
+        _log.info("导出结果到 Markdown 完成 ..........")
 
-    # -----------------
+        # -----------------
 
-    _log.info("开始分块 ..........")
-    doc = conv_result.document
+        _log.info("开始分块 ..........")
+        doc = conv_result.document
 
-    # 配置分块
-    chunker = HybridChunker()
-    chunk_iter = chunker.chunk(dl_doc=doc)
+        # 配置分块
+        chunker = HybridChunker()
+        chunk_iter = chunker.chunk(dl_doc=doc)
 
-    for i, chunk in enumerate(chunk_iter):
-        print(f"=== {i} ===")
-        print(f"chunk.text:\n{f'{chunk.text[:300]}…'!r}")
+        for i, chunk in enumerate(chunk_iter):
+            print(f"=== {i} ===")
+            print(f"chunk.text:\n{f'{chunk.text[:300]}…'!r}")
 
-        enriched_text = chunker.contextualize(chunk=chunk)
-        print(f"chunker.contextualize(chunk):\n{f'{enriched_text[:300]}…'!r}")
+            enriched_text = chunker.contextualize(chunk=chunk)
+            print(f"chunker.contextualize(chunk):\n{f'{enriched_text[:300]}…'!r}")
 
-        print()
+            print()
+
+    finally:
+        # 清理其他可能的资源...
+        pass
+
 
 if __name__ == '__main__':
     main()
