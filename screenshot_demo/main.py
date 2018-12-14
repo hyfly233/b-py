c4acb35fc13ea9888275e21de26f3337dea01348
hyfly233 <hyfly233@outlook.com>
2018-12-14 12:00:00 +0800

    commit

---
+++ screenshot_demo/main.py
@@ -0,0 +1,19 @@
+import fitz  # PyMuPDF
+from PIL import Image
+
+pdf_path = "example.pdf"
+page_number = 0  # 第1页，0为起始
+rect = (100, 100, 400, 300)  # (x0, y0, x1, y1) 左上和右下坐标
+
+# 打开PDF
+doc = fitz.open(pdf_path)
+page = doc[page_number]
+
+# 截图区域
+clip = fitz.Rect(rect)
+pix = page.get_pixmap(clip=clip, dpi=200)  # 可调整dpi提高清晰度
+
+# 保存为图片
+img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+img.save("screenshot.png")
+print("截图已保存为 screenshot.png")
