4e0a6f417a2bff0b10286c6e3d407200c706c2a7
hyfly233 <hyfly233@outlook.com>
2018-12-16 12:00:00 +0800

    commit

--- screenshot_demo/main.py
+++ screenshot_demo/main.py
@@ -1,19 +1,30 @@
+import os
+
 import fitz  # PyMuPDF
 from PIL import Image
+from dotenv import load_dotenv
 
-pdf_path = "example.pdf"
-page_number = 0  # 第1页，0为起始
-rect = (100, 100, 400, 300)  # (x0, y0, x1, y1) 左上和右下坐标
+load_dotenv()
 
-# 打开PDF
-doc = fitz.open(pdf_path)
-page = doc[page_number]
 
-# 截图区域
-clip = fitz.Rect(rect)
-pix = page.get_pixmap(clip=clip, dpi=200)  # 可调整dpi提高清晰度
+def main():
+    pdf_path = os.getenv('PDF_PATH')
+    page_number = 0  # 第1页，0为起始
+    rect = (100, 100, 400, 300)  # (x0, y0, x1, y1) 左上和右下坐标
 
-# 保存为图片
-img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
-img.save("screenshot.png")
-print("截图已保存为 screenshot.png")
+    # 打开PDF
+    doc = fitz.open(pdf_path)
+    page = doc[page_number]
+
+    # 截图区域
+    clip = fitz.Rect(rect)
+    pix = page.get_pixmap(clip=clip, dpi=200)  # 可调整dpi提高清晰度
+
+    # 保存为图片
+    img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+    img.save("screenshot.png")
+    print("截图已保存为 screenshot.png")
+
+
+if __name__ == '__main__':
+    main()
